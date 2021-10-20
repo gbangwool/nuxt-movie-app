3bababeb6da5ae5df2f8d796b249014ae9fa3480
gbangwool <mmmm1134@naver.com>
2021-10-20 19:34:31 +0900

    nuxt project

---
+++ store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'gbangwool',
+    email: 'nani9872@gmail.com',
+    github: 'https://github/gbangwool',
+    phone: '+82-10-8993-9863',
+    image: 'https://www.freepngimg.com/thumb/emoji/64910-whatsapp-bell-symbol-android-emoji-free-download-png-hq.png'
+  })
+}
